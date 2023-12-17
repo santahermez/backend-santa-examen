88c15eeb56c674ca2667c9692a68eacc5a541444
Santa Chalabi <santachalabi@hotmail.se>
2023-12-17 12:45:06 +0100

    finished

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -20,10 +20,23 @@ router.post("/register", async (req, res) => {
       return res.status(400).json({ message: "All fields are mandatory." });
     }
 
-    const existingUser = await User.findOne({ email });
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format." });
+    }
 
-    if (existingUser) {
-      return res.status(409).json({ message: "User already exists." });
+    const existingEmail = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
+
+    if (existingEmail) {
+      return res
+        .status(409)
+        .json({ message: "Email already exists.", field: "email" });
+    }
+    if (existingUsername) {
+      return res
+        .status(409)
+        .json({ message: "Username already exists.", field: "username" });
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -37,53 +50,62 @@ router.post("/register", async (req, res) => {
 
     const result = await newUser.save();
 
-    res.status(200).json({
+    return res.status(200).json({
       message: "User Created Successfully",
       result,
     });
   } catch (error) {
     console.error("Error during registration:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Login
 router.post("/login", async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { identifier, password } = req.body;
+    console.log(identifier);
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({
+      $or: [{ email: identifier }, { username: identifier }],
+    });
 
     if (!user) {
-      return res.status(404).json({ message: "Email not found" });
+      return res
+        .status(404)
+        .json({ message: "User not found", field: "identifier" });
     }
 
+    // Vänta på att bcrypt.compare ska slutföra innan du går vidare
     const passwordCheck = await bcrypt.compare(password, user.password);
 
     if (!passwordCheck) {
-      return res.status(400).json({ message: "Passwords do not match" });
+      return res
+        .status(400)
+        .json({ message: "Wrong password!", field: "password" });
     }
+    if (user && passwordCheck) {
+      user.lastLogin = new Date().toUTCString().slice(5, 22);
+      await user.save();
 
-    user.lastLogin = new Date().toUTCString().slice(5, 22);
-    await user.save();
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          userEmail: user.email,
+        },
+        JWT_SECRET,
+        { expiresIn: "24h" }
+      );
 
-    const token = jwt.sign(
-      {
-        userId: user._id,
-        userEmail: user.email,
-      },
-      JWT_SECRET,
-      { expiresIn: "24h" }
-    );
-
-    res.status(200).json({
-      message: "Login Successful",
-      _id: user._id,
-      email: user.email,
-      fullname: user.fullname,
-      username: user.username,
-      token,
-    });
+      res.status(200).json({
+        message: "Login Successful",
+        _id: user._id,
+        email: user.email,
+        fullname: user.fullname,
+        username: user.username,
+        token,
+      });
+    }
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -152,5 +174,6 @@ router.get("/image/:imageId", async (req, res) => {
 });
 
 router.get("/users", userController.getAllUsers);
+// router.get("/user/:id");
 
 module.exports = router;
